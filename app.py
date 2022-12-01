6afc7d60b5983de029e0c5b860e35a2a9c0dd63b
IvanDerdicDer <46329317+IvanDerdicDer@users.noreply.github.com>
2022-12-01 19:57:16 +0100

    FINITO

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import environ as env
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,4 +10,19 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    url = env.get("RENDER_EXTERNAL_URL")
+    port = env.get('PORT')
+
+    if url:
+        url = '127.0.0.1'
+
+    if port:
+        port = int(port)
+
+    if not url:
+        url = None
+
+    if not port:
+        port = None
+
+    app.run(host=url, port=port)
